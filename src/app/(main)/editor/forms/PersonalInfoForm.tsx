33387d15253803534869da477d5791cb20d614d9
petelkavictor <p.v.victor322@gmail.com>
2024-11-26 12:33:41 +0200

    personalinfo

---
+++ src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -0,0 +1,70 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { personalInfoSchema, personalInfoValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+export default function PersonalInfoForm() {
+  const form = useForm<personalInfoValues>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      city: "",
+      country: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async () => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+    });
+    return unsubscribe;
+  }, [form]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Personal info</h2>
+        <p className="text-sm text-muted-foreground">Tell us about yourself.</p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="photo"
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormLabel>Your photo</FormLabel>
+                <FormControl>
+                  <Input
+                    {...fieldValues}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      const file = e.target.files?.[0];
+                      fieldValues.onChange(file);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+}
